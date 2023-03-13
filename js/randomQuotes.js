d1f2cdb350cfbfd07ff8d13d641b4489c222b81a
JBreitenbr <119942939+JBreitenbr@users.noreply.github.com>
2023-03-13 23:19:21 +0100

    Create randomQuotes.js

---
+++ js/randomQuotes.js
@@ -0,0 +1,182 @@
+let quotes=[
+{ 
+  id: 1,
+  text: '"As usual, there is a great woman behind every idiot"',
+  author: 'John Lennon',
+  pic: 'imgs/Lennon.jpg'
+},
+{
+  id: 2,
+  text: '"If everyone demanded peace instead of another television set, then there\'d be peace"',
+  author: 'John Lennon',
+  pic: 'imgs/Lennon.jpg'
+},
+{
+  id: 3,
+  text: '"I mean some doctor told me I had six months to live and I went to their funeral"',
+  author: 'Keith Richards',
+  pic: 'imgs/Richards.jpg'
+},
+{
+  id: 4,
+  text: '"I\'m not getting old - I\'m evolving"',
+  author: 'Keith Richards',
+  pic: 'imgs/Richards.jpg'
+},
+{
+  id: 5,
+  text: '"You feel your strength in the experience of pain"',
+  author: 'Jim Morrison',
+  pic: 'imgs/Morrison.jpg'
+},
+{
+  id: 6,
+  text: '"Whoever controls the media, controls the mind"',
+  author: 'Jim Morrison',
+  pic: 'imgs/Morrison.jpg'
+},
+{
+  id: 7,
+  text: '"When the power of love overcomes the love of power, the world will know peace"',
+  author: 'Jimi Hendrix',
+  pic: 'imgs/Hendrix.jpeg'
+},
+{
+  id: 8,
+  text: '"Constantly choosing the lesser of two evils is still choosing evil"',
+  author: 'Jerry Garcia',
+  pic: 'imgs/Garcia.jpg'
+},
+{
+  id: 9,
+  text: '"America is still mostly xenophobic and racist. That\'s the nature of America, I think"',
+  author: 'Jerry Garcia',
+  pic: 'imgs/Garcia.jpg'
+},
+{
+  id: 10,
+  text: '"Don\'t compromise yourself. You\'re all you\'ve got"',
+  author: 'Janis Joplin',
+  pic: 'imgs/Joplin.jpeg'
+},
+{
+  id: 11,
+  text: '"Being an intellectual creates a lot of questions and no answers"',
+  author: 'Janis Joplin',
+  pic: 'imgs/Joplin.jpeg'
+},
+{
+  id: 12,
+  text: '"Tomorrow never happens. It\'s all the same fucking day, man"',
+  author: 'Janis Joplin',
+  pic: 'imgs/Joplin.jpeg'
+},
+{
+  id: 13,
+  text: '"You can destroy your now by worrying about tomorrow"',
+  author: 'Janis Joplin',
+  pic: 'imgs/Joplin.jpeg'
+},
+{
+  id: 14,
+  text: '"Hippies are so phoney and fake"',
+  author: 'George Harrison',
+  pic: 'imgs/Harrison.jpg'
+},
+{
+  id: 15,
+  text: '"I accept chaos, I\'m not sure whether it accepts me"',
+  author: 'Bob Dylan',
+  pic: 'imgs/Dylan.jpeg'
+},
+{
+  id: 16,
+  text: '"The blues are what I\'ve turned to, what has given me inspiration and relief in all the trials of my life"',
+  author: 'Eric Clapton',
+  pic: 'imgs/Clapton.jpg'
+},
+{
+  id: 17,
+  text: '"I think a lot of people came into rock\'n\'roll to try to change the world. I came into rock\'n\'roll to make music"',
+  author: 'Steve Winwood',
+  pic: 'imgs/Winwood.jpg'
+},
+{
+  id: 18,
+  text: '"You don\'t need anybody to tell you who you are or what you are. You are what you are!"',
+  author: 'John Lennon',
+  pic: 'imgs/Lennon.jpg'
+},
+{
+  id: 19,
+  text: '"I never really studied business in school. I kind of wish I had, but how boring is this?"',
+  author: 'Mick Jagger',
+  pic: 'imgs/Jagger.jpg'
+},
+{
+  id: 20,
+  text: '"As long as my face is on page one, I don\'t care what they say about me on page seventeen"',
+  author: 'Mick Jagger',
+  pic: 'imgs/Jagger.jpg'
+},
+{
+  id: 21,
+  text: '"If you want to get laid, go to college. If you want an education, go to the library"',
+  author: 'Frank Zappa',
+  pic: 'imgs/Zappa.jpeg'
+},
+{
+  id: 22,
+  text: '"Communism doesn\'t work, because people like to own stuff"',
+  author: 'Frank Zappa',
+  pic: 'imgs/Zappa.jpeg'
+},
+{
+  id: 23,
+  text: '"Art is making something out of nothing and selling it"',
+  author: 'Frank Zappa',
+  pic: 'imgs/Zappa.jpeg'
+},
+{
+  id: 24,
+  text: '"One of my favorite philosophical tenets is that people will agree with you only if they already agree with you. You do not change people\'s minds"', 
+  author: 'Frank Zappa',
+  pic: 'imgs/Zappa.jpeg'
+},
+{
+  id: 25,
+  text: '"It\'s better to burn out, than to fade away"',
+  author: 'Neil Young',
+  pic: 'imgs/Young.jpg'
+},
+{
+  id: 26,
+  text: '"It\'s always been a gift with me, hearing music the way I do. I don\'t know where it comes from, it\'s just there and I don\'t question it"',
+  author: 'Miles Davis',
+  pic: 'imgs/Davis.jpeg'
+},
+{
+  id: 27,
+  text: '"Money doesn\'t talk, it swears"',
+  author: 'Bob Dylan',
+  pic: 'imgs/Dylan.jpeg'
+},
+{
+  id: 28,
+  text: '"I like America, just as everyone does. I love America, I gotta say that. But America will be judged"',
+  author: 'Bob Dylan',
+  pic: 'imgs/Dylan.jpeg'
+},
+{
+  id: 29,
+  text: '"I\'ve been imitated so well, I\'ve heard people copy my mistakes"',
+  author: 'Jimi Hendrix',
+  pic: 'imgs/Hendrix.jpeg'
+},
+{
+  id: 30,
+  text: '"Music doesn\'t lie. If there is something to be changed in this world, then it can only happen through music"',
+  author: 'Jimi Hendrix',
+  pic: 'imgs/Hendrix.jpeg'
+}
+];
